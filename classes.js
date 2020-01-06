571c17dd3a4c969d5046c8b10c6b364bf47f365b
Timothy Barna <audioclimb@gmail.com>
2020-01-06 14:51:04 -0700

    problem  4 to go

--- classes.js
+++ classes.js
@@ -57,7 +57,18 @@ class Employee {
   Call your new class Manager
 */
 
-//Code Here
+class Manager extends Employee {
+  constructor(first_name, last_name, email, age){
+    super(first_name, last_name, email, age)
+    this.reports = [];
+  }
+  hire(employee){
+    this.reports.push(employee)
+  };
+  fire(index){
+    this.reports.splice(index, 1)
+  };
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -81,7 +92,37 @@ class Employee {
   Call your new class ProgressiveManager
 */
 
-//Code Here
+class ProgressiveManager extends Manager {
+  constructor(first_name, last_name, email, age, reports){
+    super(first_name, last_name, email, age, reports)
+    this.title = 'Not a manager';
+    this.bonus = 0;
+  }
+  titleUpdater(){
+    if (this.reports.length === 0){
+      this.title = 'Not a manager'
+    }else if (this.reports.length >= 1 && this.reports.length <= 3){
+      this.title = 'Barely Manager'
+    }else if (this.reports.length >= 4 && this.reports.length <= 10){
+      this.title = 'Mostly Manager'
+    }else if (this.reports.length >= 11 && this.reports.length <= 50){
+    this.title = 'Manager'
+    }else if (this.reports.length >= 51 && this.reports.length <= 100){
+    this.title = 'Manager Plus'
+    }else if (this.reports.length >= 101){
+    this.title = 'Bestest Manager'
+    }
+  }
+  hire(employee){
+    this.reports.push(employee)
+    this.titleUpdater()
+  };
+  fire(index){
+    this.reports.splice(index, 1)
+    this.titleUpdater()
+    this.bonus += 100
+  };
+}
 
 
 
@@ -108,6 +149,6 @@ class Employee {
         - The anonymous function should decrease wear_and_tear_count by 10, and set needs_reboot to false
 */
 
-//Code Here
+
 
 
